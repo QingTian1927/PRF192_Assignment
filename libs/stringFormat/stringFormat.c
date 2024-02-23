c5b6535bd853baa8b71694bed613bbaefb480504
QingTian <qingtian63910@proton.me>
2024-02-23 09:23:46 +0700

    doc: wrote some documentation for stringFormat.c

--- libs/stringFormat/stringFormat.c
+++ libs/stringFormat/stringFormat.c
@@ -4,10 +4,27 @@
  * @brief string formatting related functions
  */
 
+// ! NOTE: the length of a string is defined as the number of characters
+// !       within that string EXCLUDING the null terminator.
+// !
+// !       This is the same as the result of <string.h> strlen()
+
 #include "stringFormat.h"
 
 #define LOWER_UPPER_OFFSET 32 // 'a' - 'A'
 
+/**
+ * @brief Converts a lowercase ASCII letter to uppercase.
+ *
+ * Converts a lowercase letter to uppercase by subtracting the ASCII difference
+ * between these two types of letter.
+ *
+ * If the input character is not a lowercase character, simply returns the
+ * input character itself.
+ *
+ * @param ch - The character to be converted to uppercase.
+ * @return char - The uppercased character.
+ */
 char upper(char ch) {
     if (ch >= 'a' && ch <= 'z') {
         return (ch - LOWER_UPPER_OFFSET);
@@ -15,6 +32,18 @@ char upper(char ch) {
     return ch;
 }
 
+/**
+ * @brief Converts an uppercase ASCII letter to lowercase.
+ *
+ * Converts an uppercase letter to lowercase by subtracting the ASCII
+ * difference between these two types of letter.
+ *
+ * If the input character is not an uppercase character, simply returns the
+ * input character itself.
+ *
+ * @param ch - The character to be converted to lowercase.
+ * @return char - The lowercased character.
+ */
 char lower(char ch) {
     if (ch >= 'A' && ch <= 'Z') {
         return (ch + LOWER_UPPER_OFFSET);
@@ -22,14 +51,31 @@ char lower(char ch) {
     return ch;
 }
 
+/**
+ * @brief Checks if a character is a white space character.
+ *
+ * Checks whether a given character is one of the following white space
+ * characters:
+ *
+ *   *) ' '  - space
+ *   *) '\t' - horizontal tab
+ *   *) '\v' - vertical tab
+ *   *) '\f' - feed
+ *   *) '\n' - newline
+ *   *) '\r' - carriage return
+ *
+ * @param ch - The character to be checked.
+ * @return int result - 0 if is not white space.
+ *                    - 1 if is white space.
+ */
 int isWhiteSpace(char ch) {
     switch (ch) {
-        case ' ':   // space
-        case '\t':  // horizontal tab
-        case '\v':  // vertical tab
-        case '\f':  // feed
-        case '\n':  // newline
-        case '\r':  // carriage return
+        case ' ':
+        case '\t':
+        case '\v':
+        case '\f':
+        case '\n':
+        case '\r':
             return 1;
         default:
             return 0;
