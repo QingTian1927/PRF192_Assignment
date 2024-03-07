9eaed7b2e6a937d625ceaeb3226fcd3902b55c9a
QingTian <qingtian63910@proton.me>
2024-03-07 21:51:11 +0700

    feat: created graphics.c and graphics.h
    * Created a source file containing functions that print out some sort of
      graphical elements, usually as part of the user interface. Also created
      its respective header file.

---
+++ libs/graphics/graphics.c
@@ -0,0 +1,7 @@
+/**
+ * @file graphics.c
+ * @author QingTian
+ * @brief functions that print out some graphical elements
+ */
+
+#include "graphics.h"
---
+++ libs/graphics/graphics.h
@@ -0,0 +1,7 @@
+#ifndef _GRAPHICS_H_
+#define _GRAPHICS_H_
+
+#define STANDARD_PROMPT "Enter your choice: "
+#define INVALID_PROMPT "Invalid option. Please try again: "
+
+#endif
