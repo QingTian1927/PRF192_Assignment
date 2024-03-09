d6a40ad630928266038d6542cd4d73d58a692453
QingTian <qingtian63910@proton.me>
2024-03-09 08:11:34 +0700

    feat: created a template for the driver code.

---
+++ masterchefManager.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "libs/database/database.h"
+#include "libs/userinterface/userinterface.h"
+
+#include "libs/cheflist/cheflist.h"
+#include "libs/chefobject/chefobject.h"
+
+#include "libs/dateparser/dateparser.h"
+#include "libs/stringformat/stringformat.h"
+
