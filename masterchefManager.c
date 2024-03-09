49b8c90c591e59efd634b6e67608a3282277325e
QingTian <qingtian63910@proton.me>
2024-03-09 08:38:00 +0700

    feat: [THANH] added a template for the edit submenu.

--- masterchefManager.c
+++ masterchefManager.c
@@ -11,6 +11,42 @@
 #include "libs/dateparser/dateparser.h"
 #include "libs/stringformat/stringformat.h"
 
+void editChefWrapper() {
+    int hasNotExited = 1;
+    int isInvalidOption = 0;
+
+    while (hasNotExited) {
+        clearScreen();
+        printTitleCard();
+
+        if (isInvalidOption == 0) {
+            printEditSubmenu(STANDARD_PROMPT);
+        } else {
+            printEditSubmenu(INVALID_PROMPT);
+            isInvalidOption = 0;
+        }
+
+        char choice = getchar();
+        printf("\n");
+
+        switch (choice) {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '0':
+                hasNotExited = 0;
+                break;
+            default:
+                isInvalidOption = 1;
+        }
+    }
+}
+
 int main() {
     int hasNotExited = 1;
     int isInvalidOption = 0;
